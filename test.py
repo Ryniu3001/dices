48299dc571e75e14d05db3fbf555d2e836a10e4b
Ryniu <marcin3001@10g.pl>
2014-11-27 21:28:27 +0100

    skutecznosc 82 % w pliku test.py

--- test.py
+++ test.py
@@ -2,48 +2,103 @@ from __future__ import print_function
 import os
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import ndimage
+import math
 import cv, cv2
+
+def gamma_correction(img, correction):
+    img = img/255.0
+    img = cv2.pow(img, correction)
+    return np.uint8(img*255)
+
+
+
 def main():
-    plt.figure(figsize=(16 , 18))                # dla jednej kostki (16, 18) dla zbioru (50, 60)
+    PLIKI = 5                                  # ktore pliki zaladowac
+    plt.figure(figsize=(40, 50))                # dla jednej kostki (16, 18) dla zbioru (50, 60)
     dices = []                                  #
-    #for file in os.listdir("./kostki/"):        #
-     #  if file.endswith("(6).jpg"):              #
-    #      dices += ["./kostki/" + file]       # Zbior kostek
-    dices = ["./kostki/76(3).jpg"]             # jedna kostka
-    print (len(dices))
+    for file in os.listdir("./kostki/"):        #
+     if file.endswith("(%d).jpg" % PLIKI):              #
+          dices += ["./kostki/" + file]       # Zbior kostek
+    #dices = ["./kostki/33(5).jpg"]             # jedna kostka
+    print ("Ilosc zdjec: ",len(dices))
     i = 0
     verify = 0
     for file in dices:
-        ax = plt.subplot(1, 2, i)
+        print ("Zdjecie: ",file)
+        ax = plt.subplot(math.ceil(len(dices)/2.0), 2, i)
 
         oryg = cv2.imread(file)
         grey = cv2.cvtColor(oryg, cv.CV_RGB2GRAY)
+        grey = gamma_correction(grey,1.5)
         grey = cv2.fastNlMeansDenoising(grey, None, 11, 7, 21)  #znaczaco wydluza obliczenia
         grey = cv2.medianBlur(grey, 5)
         edges = cv2.Canny(grey,90,120)
         kernel = np.ones((1,1),np.uint8)
         dil = cv2.dilate(edges, kernel,iterations = 2)
-        dil = cv2.morphologyEx(dil, cv2.MORPH_OPEN, kernel)
+        th2 = cv2.morphologyEx(dil, cv2.MORPH_OPEN, kernel)
+
+        repeat = 0
+
+        (cnts, _) = cv2.findContours(th2.copy() , cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+        screenCnt = None
 
 
-        circles =  cv2.HoughCircles(dil, cv2.cv.CV_HOUGH_GRADIENT, 2, 30, np.array([]), 30, 45, 5, 40)
+        cnts = cnts[repeat:]
+
+        for c in cnts:
+            # approximate the contour
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+            # if our approximated contour has four points, then
+            # we can assume that we have found our screen
+            #print (len(approx))
+            if(max(approx[:,0,0]) - min(approx[:,0,0]) < 900):
+                screenCnt = approx
+                break
+
+        #cv2.drawContours(oryg, [screenCnt], -1, (0,255,0), 3)
+
+        a = 200
+        h = len(oryg)                #wyoskosc
+        w = len(oryg[0])               #szerokosc
+        xmax = max(screenCnt[:, 0, 0]) + a
+        xmin = min(screenCnt[:, 0, 0]) - a
+        ymax = max(screenCnt[:, 0, 1]) + a
+        ymin = min(screenCnt[:, 0, 1]) - a
+
+        xmin = 0 if xmin<0 else xmin
+        ymin = 0 if ymin<0 else ymin
+        ymax = h if ymax>h else ymax
+        xmax = w if xmax>w else xmax
+
+        small_image = oryg[ymin:ymax,xmin:xmax]
+
+        if len(small_image) == 0:
+            print("Brak rozpoznania")
+            continue
+
+        th5 = cv2.blur(th2[ymin:ymax,xmin:xmax],(3,3))
+        #th5 = cv2.adaptiveThreshold(th5, 10, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 19, 3)
+
+        circles =  cv2.HoughCircles(th5, cv2.cv.CV_HOUGH_GRADIENT, 1, 30, np.array([]), 10, 30, 5, 60)
         if circles is not None:
             for c in circles[0]:
-                cv2.circle(oryg, (c[0],c[1]), c[2], (0,255,0),2)
+                cv2.circle(small_image, (c[0],c[1]), c[2], (0,255,0),2)
                 #edges = cv2.Canny( blur, 40, 80 )
-            if len(circles[0]) == 6:
+            print(circles[0])
+            if len(circles[0]) == int(file[-6]):
                 verify += 1
-        ax.imshow(oryg, cmap=plt.get_cmap("gray"))
-        plt.subplot(1,2,i+1), plt.imshow(dil, cmap=plt.get_cmap("gray"))
+
+        ax.imshow(small_image)
+        #plt.subplot(1,2,i+1), plt.imshow(oryg)
         ax.axis('off')
         ax.set_title(file)
         i += 1
     print (str(verify) + " / " + str(len(dices)))
-    #plt.savefig('countours6.pdf')
-    plt.show()
-
+    plt.savefig('countours%d.pdf' % PLIKI)
+    #plt.show()
 
 if __name__ == '__main__':
     main()
-
